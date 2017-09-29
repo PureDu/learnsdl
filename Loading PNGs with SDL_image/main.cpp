33536d7f541b26ab75efd10756d8afa6061ba973
duxiaobo <848295325@qq.com>
2017-09-29 16:57:18 +0800

    Loading PNGs with SDL_image

--- Loading PNGs with SDL_image/main.cpp
+++ Loading PNGs with SDL_image/main.cpp
@@ -1,6 +1,8 @@
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
+#include <string>
 
 
 #undef main
@@ -12,12 +14,13 @@ const int SCREEN_HEIGHT = 480;
 bool init();
 bool loadMedia();
 void close();
+SDL_Surface* loadSurface(std::string path);
 
 
 
 SDL_Window* gWindow = nullptr;
 SDL_Surface* gScreenSurface = nullptr;
-SDL_Surface* gXSurface = nullptr;
+SDL_Surface* gPNGSurface = nullptr;
 
 
 int main(int argc, char* args[]) {
@@ -29,11 +32,9 @@ int main(int argc, char* args[]) {
 		if (!loadMedia()) {
 			printf("Failed to load media!\n");
 		} else {
-			SDL_BlitSurface(gXSurface, nullptr, gScreenSurface, nullptr);
-			SDL_UpdateWindowSurface(gWindow);
-
 			SDL_Event e;
 			bool quit = false;
+
 			while (!quit) {
 
 				while (SDL_PollEvent(&e) != 0) {
@@ -41,6 +42,9 @@ int main(int argc, char* args[]) {
 						quit = true;
 					}
 				}
+
+				SDL_BlitSurface(gPNGSurface, nullptr, gScreenSurface, nullptr);
+				SDL_UpdateWindowSurface(gWindow);
 			}
 		}
 	}
@@ -64,6 +68,12 @@ bool init() {
 			printf("Window could not be crated! SDL_Error %s\n", SDL_GetError());
 			success = false;
 		} else {
+
+			int imageFlags = IMG_INIT_PNG;
+			if (!(IMG_Init(imageFlags) & imageFlags)) {
+				printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+				success = false;
+			}
 			gScreenSurface = SDL_GetWindowSurface(gWindow);
 		}
 	}
@@ -74,20 +84,39 @@ bool init() {
 
 bool loadMedia() {
 	bool success = true;
-	gXSurface = SDL_LoadBMP("x.bmp");
-	if (gXSurface == nullptr) {
-		printf("Unable to load image %s ! SDL Error: %s \n", "x.bmp", SDL_GetError());
+	gPNGSurface = loadSurface("loaded.png");
+	if (gPNGSurface == nullptr) {
+		printf("Failed to load PNG image!\n");
 		success = false;
 	}
 
 	return success;
 }
 
+SDL_Surface* loadSurface(std::string path) {
+	SDL_Surface* optimizedSurface = nullptr;
+
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+	if (loadedSurface == nullptr) {
+		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+	} else {
+		optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
+		if (optimizedSurface == nullptr) {
+			printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+		}
+
+		SDL_FreeSurface(loadedSurface);
+	}
+
+	return optimizedSurface;
+}
+
 void close() {
-	SDL_FreeSurface(gXSurface);
-	gXSurface = nullptr;
+	SDL_FreeSurface(gPNGSurface);
+	gPNGSurface = nullptr;
 	SDL_DestroyWindow(gWindow);
 	gWindow = nullptr;
 
+	IMG_Quit();
 	SDL_Quit();
 }
