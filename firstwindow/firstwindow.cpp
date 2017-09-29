23ae38e3c55a306cfdeae3603e161b0376f4900c
duxiaobo <848295325@qq.com>
2017-09-29 14:11:40 +0800

    添加项目文件。

---
+++ firstwindow/firstwindow.cpp
@@ -0,0 +1,39 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+#undef main
+
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+
+int main(int argc, char* args[]) {
+	SDL_Window* window = nullptr;
+
+	SDL_Surface* screenSurface = nullptr;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	} else {
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == nullptr) {
+			printf("Window could not be crated! SDL_Error %s\n", SDL_GetError());
+		} else {
+			screenSurface = SDL_GetWindowSurface(window);
+			SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xff, 0xff, 0xff));
+
+			SDL_UpdateWindowSurface(window);
+
+			SDL_Delay(2000);
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
+
